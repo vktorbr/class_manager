49bd153e330ad7ec3ec3e69e3af0ef24bcec0a8d
vktorbr <vktorbr@gmail.com>
2020-09-05 01:17:42 -0300

    formulario para cadastro de professores

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const teachers = require('./teachers');
 //rotas
 routes.get('/', function(req, res){
     return res.redirect('/teachers');
@@ -9,6 +9,12 @@ routes.get('/', function(req, res){
 routes.get('/teachers', function(req, res){
     return res.render('teachers/index');
 })
+
+routes.get('/teachers/create', function(req, res){
+    return res.render('teachers/create');
+})
+
+routes.post('/teachers', teachers.post);
 
 routes.get('/students', function(req, res){
     return res.send('students');
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const routes = require('./routes');
 
 const server = express();
 
+//req.body
+server.use(express.urlencoded({extended: true}));
 //pega arquivos statics(como css) da pasta public 
 server.use(express.static('public'));
 server.use(routes);
